b1558c510ccdc3655f68d61b1e264a70f7d4f340
Ben Hamner <ben@benhamner.com>
2013-08-15 14:12:03 -0700

    updated ingest

--- PostgresIngest/exploratoryQueries.sql
+++ PostgresIngest/exploratoryQueries.sql
@@ -1,19 +1,26 @@
 
-SELECT COUNT(*)
+SELECT COUNT(visitor_id) -- 16,118,859
 FROM UserSearch
 
-SELECT COUNT(DISTINCT userid)
+SELECT COUNT(DISTINCT visitor_id) -- 608616
 FROM UserSearch
 
-SELECT UserId,
-       COUNT(UserId)
+SELECT visitor_id,
+       COUNT(visitor_id)
 FROM UserSearch
-GROUP BY UserId
-ORDER BY COUNT(UserId) DESC
+GROUP BY visitor_id
+ORDER BY COUNT(visitor_id) DESC
 
-SELECT UserId,
+SELECT srch_id,
+       COUNT(srch_id)
+FROM UserSearch
+GROUP BY srch_id
+ORDER BY COUNT(srch_id) DESC
+
+SELECT visitor_id,
        SUM(click_bool),
-       SUM(order_bool)
+       SUM(booking_bool),
+       COUNT(DISTINCT srch_id)
 FROM UserSearch
-GROUP BY UserId
+GROUP BY visitor_id
 ORDER BY SUM(click_bool) DESC
